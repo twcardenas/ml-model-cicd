e30da26ca5a0b109fcfaa5aa9b4a7cf76909d44b
Thomas Cardenas <twcardenas@outlook.com>
2022-08-22 09:20:28 -0600

    Delete split_dataset.py

--- split_dataset.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-matches = pd.read_csv("processed_data.csv", index_col=0)
-
-train = matches[matches["date"] < '2022-01-01']
-test = matches[matches["date"] > '2022-01-01']
-
-train.to_csv("./train_split.csv")
-test.to_csv("./test_split.csv")
